b6fbc39d5a81549dfb7481b24a9641359719bb0a
paghdalparth <paghdalparth22@gmail.com>
2025-05-26 08:59:58 +0530

    db

--- frontend/src/socekt.js
+++ frontend/src/socekt.js
@@ -1,5 +1,5 @@
 // src/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000/'); // Make sure this matches your backend port
+const socket = io('https://ful2win-backend.onrender.com'); // Make sure this matches your backend port
 export default socket;
