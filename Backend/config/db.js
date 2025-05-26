13c2f54bfa1edc7a3e51eb5d12287cb6ccd802a6
paghdalparth <paghdalparth22@gmail.com>
2025-05-26 08:51:19 +0530

    db

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,14 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://paghdalparth22:vvNLtvmfp9K6Sget@cluster0.puiareb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-    // await mongoose.connect('mongodb+srv://boostnowchat:Boost@123@cluster0.mx0e6m5.mongodb.net/gameDB', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb+srv://paghdalparth22:vvNLtvmfp9K6Sget@cluster0.puiareb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('MongoDB connected');
   } catch (err) {
-    console.error(err.message);
+    console.error('MongoDB connection error:', err.message);
     process.exit(1);
   }
 };
