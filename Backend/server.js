a54b5f35a7510baf8517cd7acca0e811a4e982b3
pratikpaliwal355@gmail.com <pratikpaliwal355@gmail.com>
2025-05-30 13:56:20 +0530

    push routes in server

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 // Setup Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://ful2win.onrender.com"],  // Your deployed frontend URL
+    origin: ["http://localhost:3000","http://localhost:5173",  "https://ful2win.onrender.com"],  // Your deployed frontend URL
     methods: ["GET", "POST", "PUT"],
   },
 });
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors({
-  origin: ["http://localhost:3000", "https://ful2win.onrender.com"]
+  origin: ["http://localhost:3000","http://localhost:5173",  "https://ful2win.onrender.com"]
 }));
 app.use(express.json());
 
