0a6f7fe96ff75a5fd0547f45ce8ca93651224e92
pratikpaliwal355@gmail.com <pratikpaliwal355@gmail.com>
2025-06-08 12:30:24 +0530

    db

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/gameDB');
-    // await mongoose.connect('mongodb+srv://paghdalparth22:vvNLtvmfp9K6Sget@cluster0.puiareb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    // await mongoose.connect('mongodb://localhost:27017/gameDB');
+    await mongoose.connect('mongodb+srv://paghdalparth22:vvNLtvmfp9K6Sget@cluster0.puiareb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
